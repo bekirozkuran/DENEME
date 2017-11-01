0cd56fa4c595e750f4e489eb86a44c271fbebb8b
bekirozkuran <bekir.ozkuran@isik.edu.tr>
2017-11-01 12:17:26 +0200

    string b

--- deneme/src/deneme/Deneme.java
+++ deneme/src/deneme/Deneme.java
@@ -16,7 +16,7 @@ public class Deneme {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        HELLO
+        String b = "HELLO";
     }
     
 }
